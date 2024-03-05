38bd425a85aa1a18b2cc1e0cdcaa0c430fb1de9f
Andre Soares Gomes <andrsoares953@yahoo.com>
2024-03-05 20:04:19 -0300

    [Feat] - Added example to use ngmask

--- Projects/RXJS/src/app/app.module.ts
+++ Projects/RXJS/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { NgxMaskDirective, NgxMaskPipe, provideEnvironmentNgxMask } from 'ngx-mask';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    NgxMaskDirective, 
+    NgxMaskPipe
   ],
   declarations: [
       AppComponent,
       RegisterComponent
+  ],
+  providers: [
+    provideEnvironmentNgxMask(),
   ],
   bootstrap: [AppComponent],
 })
