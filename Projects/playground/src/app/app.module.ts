e24567631a4b261791f003d4f095f514faecf89e
Andre Soares Gomes <andrsoares953@yahoo.com>
2024-05-15 20:36:01 -0300

    [Feat] - Implemented an example of :host and ::ng-deep

--- Projects/playground/src/app/app.module.ts
+++ Projects/playground/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { H2Component } from './components/h2/h2.component';
 import { HostSelectorComponent } from './components/host-selector/host-selector.component';
 import { BorderHighlightDirectiveDirective } from './directives/border-highlight-directive/border-highlight-directive';
 import { HoverHighlightDirective } from './directives/hover-highlight/hover-highlight.directive';
@@ -12,6 +13,7 @@ import { NgUnlessDirective } from './directives/ngUnless.directive';
   declarations: [
     AppComponent,
     HostSelectorComponent,
+    H2Component,
     HoverHighlightDirective,
     BorderHighlightDirectiveDirective,
     NgUnlessDirective
