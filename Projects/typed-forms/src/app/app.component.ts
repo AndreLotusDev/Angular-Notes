e04b585bbfafdc2fc49db5901a3c4bf0f1045f50
Andre Soares Gomes <andrsoares953@yahoo.com>
2024-06-25 08:36:01 -0300

    [Feat] - Added the difference between typed and untyped

--- Projects/typed-forms/src/app/app.component.ts
+++ Projects/typed-forms/src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, Inject } from '@angular/core';
-import { UntypedFormBuilder } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  NonNullableFormBuilder,
+  UntypedFormBuilder,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,8 +14,48 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  untypedFormService = Inject(UntypedFormBuilder);
+export class AppComponent implements OnInit {
+  untypedFormService = inject(UntypedFormBuilder);
+  typedFormService = inject(FormBuilder);
+
+  typedForm = this.typedFormService.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    age: ['', Validators.required],
+    gender: ['', Validators.required],
+  });
+
+  nonNullableFormService = inject(NonNullableFormBuilder);
+
+  nonNullableForm = this.nonNullableFormService.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    age: ['', Validators.required],
+    gender: ['', Validators.required],
+  });
 
   constructor() {}
+  ngOnInit(): void {
+    this.generateForm();
+  }
+
+  private generateForm() {
+    //Using typed forms
+    const typedForm = this.typedFormService.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      age: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+
+    const typedFormValues = typedForm.value;
+    console.log(typedFormValues.name);
+
+    const untypedForm = this.untypedFormService.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      age: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+  }
 }
